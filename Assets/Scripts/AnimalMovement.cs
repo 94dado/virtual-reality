c507ae820a3e09f794994dcd4cfbdf4100f9f5ee
Davide Quadrelli <ildado94@gmail.com>
2017-10-22 16:48:37 +0200

    Updated prefabs of Animals
    Bug fixed Chicken and other movement.

---
+++ Assets/Scripts/AnimalMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AnimalMovement : MonoBehaviour {
+
+    public Animator animator;
+    private NavMeshAgent navigator;
+	
+	// Update is called once per frame
+	void Start () {
+        animator = GetComponent<Animator>();
+        navigator = GetComponent<NavMeshAgent>();
+        
+	}
+}
