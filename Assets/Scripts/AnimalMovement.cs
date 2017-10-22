61c2c2d9359e0c2c2adf0eb28c8a5f42744f85ca
Davide Quadrelli <ildado94@gmail.com>
2017-10-22 17:16:56 +0200

    Added animal movement script
    Not yet tested. Waiting for other development of code

--- Assets/Scripts/AnimalMovement.cs
+++ Assets/Scripts/AnimalMovement.cs
@@ -1,17 +1,37 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class AnimalMovement : MonoBehaviour {
 
+    //custom settings
+    public float idleTime;
+
     public Animator animator;
     private NavMeshAgent navigator;
+
 	
 	// Update is called once per frame
 	void Start () {
         animator = GetComponent<Animator>();
         navigator = GetComponent<NavMeshAgent>();
-        
+        StartCoroutine(MovementRoutine());
 	}
+
+    private IEnumerator MovementRoutine() {
+        while (true) {
+        //animal just spawned or stopped. Wait some times
+        yield return new WaitForSeconds(idleTime);
+        //choose destination
+        Vector3 destination = new Vector3(0, 0, 1);    //TODO call method to change them
+                                                        //set destination and start animation
+        animator.SetBool("Move", true);
+        navigator.SetDestination(destination);
+        while (destination.x != transform.position.x && destination.z != transform.position.z) {
+            yield return 0;
+        }
+        //destination reached. Stop animation
+        animator.SetBool("Move", false);
+        }
+    }
 }
