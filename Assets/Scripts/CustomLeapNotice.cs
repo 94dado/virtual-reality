3bedd6f24f99b864e3d3b83fe853bbec609406e6
Davide Quadrelli <ildado94@gmail.com>
2017-11-09 11:12:36 +0100

    Leap Motion support complete.
    The leap motion notice is not working, also created a simple custom notice. Everything is working

---
+++ Assets/Scripts/CustomLeapNotice.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using Leap;
+
+public class CustomLeapNotice : MonoBehaviour {
+
+    public int frameDelay = 10;
+
+    Controller leapController;
+    int frameCounter;
+    Canvas canvas;
+	// Use this for initialization
+	void Start () {
+        leapController = new Controller();
+        frameCounter =  0;
+        canvas = GetComponent<Canvas>();
+        canvas.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(IsConnected() && canvas.enabled) {
+            //leap connected and canvas is visible. Hide canvas
+            canvas.enabled = false;
+            frameCounter = 0;
+        }
+        else if(!IsConnected()){
+            frameCounter++;
+            if(frameCounter > frameDelay) {
+                canvas.enabled = true;
+            }
+        }
+	}
+
+    bool IsConnected()
+    {
+        return leapController.IsConnected;
+    }
+}
