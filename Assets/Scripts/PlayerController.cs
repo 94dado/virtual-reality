33d2bf09fa8314d7fcd271a481ab3fb2c89e3621
Davide Quadrelli <ildado94@gmail.com>
2017-11-09 16:29:11 +0100

    Writed gesture recognition code
    Not tested yet with Leap Motion Controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,40 +2,51 @@
 using Leap;
 
 /*LIST OF ALL GESTURES:
- * Create fire-ball: open palm facing up
- * Shoot fire-ball: closed hand, palm facing down
- * Thunder: two pointing fingers (directed forward)
- * Missile: palm facing left, two fingers (one directed up, one forward)
- * Flamethrower: open palm facing forward
- * Aim enemy: one pointing fingers (directed forward)
+ * 1) Create fire-ball:            closed hand, palm facing up                                          E
+ * 2) Shoot fire-ball:             open palm facing up                                                  R
+ * 3) Create thunder:              closed hand, palm facing down                                        Q
+ * 4) Shoot thunder:               open palm facing down                                                tab
+ * 5) Missile:                     palm facing left, two fingers (one directed up, one forward)         F
+ * 6) Flamethrower:                open palm facing forward                                             Space
+ * 7) Aim enemy:                   one pointing fingers (directed forward)                              LeftMouse
  */
 
 public class PlayerController : MonoBehaviour {
-
-    //leap motion settings
-    public float aimMinVelocity = 50.0f;            // mm/s
-    public float aimHistorySeconds = .1f;           // s
-    public float aimMinDistance = 5.0f;             // mm
-
-    public float flameRadius = 5.0f;                // mm
+    //public
+    bool useLeapMotion = true;
 
     //private attributes
     Controller controller;              //for leap
     bool aiming;             //i'm aiming an enemy?
 
-	void Start () {
+    void Start() {
         //create the leap controller
         controller = new Controller();
     }
-	
 
-	void Update () {
-        //get frame and gestures
-        Frame frame = controller.Frame();
-        //search for a custom gesture
-        if(frame.Hands.Count > 0)
-            SearchCustomGesture(frame);
-	}
+
+    void Update() {
+        if (useLeapMotion) {
+            //get frame and gestures
+            Frame frame = controller.Frame();
+            //search for a custom gesture
+            if (frame.Hands.Count > 0)
+                SearchCustomGesture(frame);
+        }
+        //when i'm not using leap motion, everything is on the keyboard
+        else {
+            //TODO write code to test every action that the player can do
+        }
+        
+    }
+
+    //return true if the player is making a gun with his fingers (only two finger: one finger directed up and the other forward)
+    bool FingersLikeGun(FingerList pointingFingers) {
+        if (pointingFingers.Count != 2) return false;
+        Vector v1 = GetVectorDirection(pointingFingers[0].Direction),
+               v2 = GetVectorDirection(pointingFingers[1].Direction);
+        return ( v1 == Vector.Up && v2 == Vector.Forward) || (v2 == Vector.Up && v1 == Vector.Forward);
+    }
 
     //get data from leap and check if a custom gesture is triggered
     void SearchCustomGesture(Frame frame) {
@@ -46,8 +57,43 @@ public class PlayerController : MonoBehaviour {
         //get the direction of the palm
         Vector palmDirection = GetVectorDirection(hand.PalmNormal);
         //TODO use the data that we have to recognize the gestures, even the "no gestures event"
-        Debug.Log("Pointing Fingers: " + pointingFingers.Count);
-        Debug.Log("Palm direction: " + palmDirection);
+
+        //closed hand, palm facing up - create fireball
+        if (pointingFingers.Count == 0 && palmDirection == Vector.Up) {
+            Debug.Log("Create fire-ball");
+            //TODO create fireball
+        }
+        //open palm facing up - shoot fireball
+        else if (pointingFingers.Count == 5 && palmDirection == Vector.Up) {
+            //TODO shoot fireball
+            Debug.Log("Shoot fire-ball");
+        }
+        //closed hand, palm facing down - create thunder
+        else if (pointingFingers.Count == 0 && palmDirection == Vector.Down) {
+            //TODO create thunder
+            Debug.Log("Create thunder");
+        }
+        //open palm facing down - shoot thunder
+        else if (pointingFingers.Count == 5 && palmDirection == Vector.Down) {
+            //TODO launch thunder
+            Debug.Log("Shoot thunder");
+        }
+        //palm facing left, two fingers - missile
+        else if(FingersLikeGun(pointingFingers) && palmDirection == Vector.Left) {
+            //TODO shoot missile
+            Debug.Log("Shoot missile");
+        }
+        //open palm facing forward - flamethrower
+        else if(pointingFingers.Count == 5 && palmDirection == Vector.Forward) {
+            //TODO start flamethrowing
+            Debug.Log("Flamethrower");
+        }
+        //one pointing finger forward - aim
+        else if (pointingFingers.Count == 1 && palmDirection == Vector.Left) {
+            //TODO aim
+            Debug.Log("Aiming");
+        }
+
     }
 
     //return the rounded normal values of the palm of an hand (usefull to confront them with directions)
@@ -56,12 +102,5 @@ public class PlayerController : MonoBehaviour {
         float y = Mathf.Round(vec.y);
         float z = Mathf.Round(vec.z);
         return new Vector(x, y, z);
-    }
-
-
-
-    // attack
-    void Attack() {
-        
     }
 }
